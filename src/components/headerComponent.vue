3525d2880270d8dd6d60b8c185ce964cc33dcc32
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-31 01:47:12 +0200

    added header dropdown menu

--- src/components/headerComponent.vue
+++ src/components/headerComponent.vue
@@ -1,31 +1,27 @@
 <script>
+import headerMenu from "./headerMenu.vue";
 export default {
   name: "headerComponent",
+  components: {
+    headerMenu,
+  },
 };
 </script>
 <template>
   <header>
-    <div class="container">
-      <!-- imposta la larghezza a 25%  categorie uomo donna bambino-->
-      <div class="col-25">
-        <div class="categories">
-          <button class="category">Uomo</button>
-          <button class="category">Donna</button>
-          <button class="category">Bambino</button>
-        </div>
-      </div>
-      <!-- larghezza 50% logo al centro della pagina -->
-      <div class="col-50 h-logo">
-        <img class="logo" src="/img/boolean-logo.png" alt="" />
-      </div>
-      <!-- bottoni carello, accedi e preferiti -->
-      <div class="col-25 buttons">
-        <button class="button">&hearts;</button>
-        <button class="button shop">
-          <img class="shopp" src="/img/carrello.png" alt="" />
-        </button>
-        <button class="button">&#128100;</button>
-      </div>
+    <!-- imposta la larghezza a 25%  categorie uomo donna bambino-->
+    <div class="col-25">
+      <headerMenu />
+    </div>
+    <!-- larghezza 50% logo al centro della pagina -->
+    <div class="col-50 h-logo">
+      <img class="logo" src="/img/boolean-logo.png" alt="" />
+    </div>
+    <!-- bottoni carello, accedi e preferiti -->
+    <div class="col-25 buttons">
+      <button class="prefered">&hearts;</button>
+      <button class="shop"></button>
+      <button class="button">&#128100;</button>
     </div>
   </header>
 </template>
@@ -34,5 +30,16 @@ button {
   background: orange;
   border: none;
   color: white;
+}
+header {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+.h-logo img {
+  width: 50%;
+}
+.buttons {
+  text-align: right;
 }
 </style>
---
+++ src/components/headerMenu.vue
@@ -0,0 +1,102 @@
+<script>
+export default {
+  name: "menuHeader",
+  data() {
+    return {
+      listMenu: [
+        {
+          gender: "Uomo",
+          show: false,
+
+          categories: [
+            "T-shirt & Polo",
+            "Felpe",
+            "Jeans",
+            "Pantaloni",
+            "Giacche",
+            "Tutto l'abbigliamento",
+            "Camicie",
+            "Pantaloni sportivi e Joggers",
+            "Bermuda",
+            "Moda mare",
+            "Completi e cravatte",
+          ],
+        },
+        {
+          gender: "Donna",
+          show: false,
+
+          categories: [
+            "T-shirt & Polo",
+            "Felpe",
+            "Jeans",
+            "Pantaloni",
+            "Giacche",
+            "Tutto l'abbigliamento",
+            "Camicie",
+            "Pantaloni sportivi e Joggers",
+            "Bermuda",
+            "Moda mare",
+            "Completi e cravatte",
+          ],
+        },
+        {
+          gender: "Bambini",
+          show: false,
+          categories: [
+            "T-shirt & Polo",
+            "Felpe",
+            "Jeans",
+            "Pantaloni",
+            "Giacche",
+            "Tutto l'abbigliamento",
+            "Camicie",
+            "Pantaloni sportivi e Joggers",
+            "Bermuda",
+            "Moda mare",
+            "Completi e cravatte",
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
+<template>
+  <div class="gender">
+    <div class="genderContainer">
+      <div class="dropDawnMenu" v-for="item in listMenu">
+        <span @click="item.show = !item.show">{{ item.gender }}</span>
+        <ul v-if="item.show">
+          <li v-for="category in item.categories">{{ category }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.genderContainer {
+  display: flex;
+  color: white;
+}
+ul {
+  top: 100%;
+  list-style-type: none;
+  position: absolute;
+  background-color: gray;
+
+  left: 0;
+  right: 0;
+  z-index: 2;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 10px;
+}
+li {
+  margin: 10px 0;
+  width: calc(100% / 5);
+}
+.dropDawnMenu {
+  margin: 0 10px;
+}
+</style>
